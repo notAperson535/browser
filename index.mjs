e47afa64924d757341d6801bb66ae9f92a14ed30
830008 <nrollins189@gmail.com>
2023-01-20 17:07:54 -0600

    Adding Submodule

--- index.mjs
+++ index.mjs
@@ -27,3 +27,102 @@ server.on('upgrade', (req, socket, head) => {
 server.listen({
 	port: process.env.PORT || 8080,
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default class {
+	prefix = "";
+	wsPrefix = "";
+	debug = true;
+
+	constructor(prefix, wsPrefix, debug) {
+		this.prefix = prefix;
+		this.wsPrefix = wsPrefix;
+		this.debug = debug ?? false;
+	}
+	route(path) {
+		return path === this.prefix;
+	}
+	routeWs(path) {
+		return path.startsWith(this.wsPrefix);
+	}
+	async handle(req) {
+		const url = req.headers.get("x-url") || "";
+
+		// deno-lint-ignore ban-types
+		const headers = JSON.parse(req.headers.get("x-headers") || "");
+
+		if (this.debug) console.log(`Handling ${url}`);
+
+		try {
+			const opts = {
+				method: req.method,
+				headers: headers,
+			};
+
+			if (opts.method === "POST") {
+				opts.body = await req.text();
+
+				console.log(`${req.method} ${url}`);
+			}
+
+			const proxyResp = await fetch(url, opts);
+
+			const respHeaders = Object.fromEntries(proxyResp.headers.entries());
+
+			// Don't cache
+			delete respHeaders["age"];
+			delete respHeaders["cache-control"];
+			delete respHeaders["expires"];
+
+			return new Response(await proxyResp.body, {
+				status: proxyResp.status,
+				headers: {
+					"cache-control": "no-cache",
+					...respHeaders,
+				},
+			});
+		} catch (err) {
+			return new Response(err.message, { status: 500 });
+		}
+	}
+	handleWs(req) {
+		let resp, sock = nul;
+
+		const proto = req.headers.get("sec-websocket-protocol") || "";
+
+		try {
+			({ response: resp, socket: sock } = Deno.upgradeWebSocket(req, {
+				protocol: proto,
+			}));
+		} catch {
+			return new Response("Not a WS connection");
+		}
+
+		const url = new URL(req.url).searchParams.get("url") || "";
+
+		if (this.debug) console.log(`Handling WS ${url}`);
+
+		const proxySock = new WebSocket(url, proto);
+
+		sock.onmessage = e => proxySock.send(e.data);
+		proxySock.onmessage = e => sock.send(e.data);
+
+		sock.onclose = () => proxySock.close();
+		proxySock.onclose = () => sock.close();
+
+		return resp;
+	}
+}
--- public/backend/workerApis/sw.js
+++
@@ -1,7 +0,0 @@
-// Service worker nests
-
-importScripts = new Proxy(importScripts, {
-	apply(target, that, args) {
-		Reflect.apply(...arguments);
-	},
-});
