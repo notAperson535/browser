5844e10b03af957db13ea1da4c2be511f4fe3839
notAperson535 <notAperson939@gmail.com>
2023-01-21 16:45:33 -0500

    stuff

--- public/sw-register.js
+++ public/sw-register.js
@@ -1,5 +1,6 @@
-import { proxyApi } from "./backend/config";
-navigator.serviceWorker.register("sw.js", { scope: proxyApi.prefix}).then(function (registration) {
+import { proxyApi } from "./backend/config.js";
+console.log("hi")
+navigator.serviceWorker.register("./sw.js", { scope: proxyApi.prefix }).then(function (_registration) {
     console.log("Service worker registered");
 })
 
--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 import handle from "./backend/handle.js";
 import dynamicUpdates from "./backend/updates.js";
 
-self.addEventListener("install", event => self.skipWaiting());
+self.addEventListener("install", _event => self.skipWaiting());
 
-self.addEventListener("fetch", async event =>
+self.addEventListener("fetch", event =>
 	event.respondWith(
 		handle(event).catch(err => new Response(err.stack, { status: 500 }))
 	)
